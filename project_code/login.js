758d8a8c83b0c7640a8e1ea4020d8b88df661599
nenevadera <yjinjin2001@gmail.com>
2022-11-09 16:49:01 -0500

    Add Login Function

--- project_code/login.js
+++ project_code/login.js
@@ -8,5 +8,17 @@ function profile(){
 }
 
 // Login functions
-function login(){}
-//Yujin is trying to commit.
+function login() {
+    const name = document.getElementById("login_name").value;
+    const password = document.getElementById("password").value;
+    // const user = { "id": name, "user_name": password };
+    //Should we check if the username exists and password matches??
+    if ((name && name.length > 0) && (password && password.length > 0)) {
+        localStorage.setItem('login_name', name);
+        localStorage.setItem('password', password);
+        localStorage.setItem('fromPage', 'login');
+        window.location.href = "./index.html";
+    } else {
+        alert("Please enter valid username and password. If you don't have an account, please create one.");
+    }
+}
