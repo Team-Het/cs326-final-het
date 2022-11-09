dbafb37093837407d26bb7cd5269061d96f05ec9
Yuqi Liu <yuqliu@umass.edu>
2022-11-09 15:16:58 -0500

    add api and functions

--- project_code/database.js
+++ project_code/database.js
@@ -63,9 +63,53 @@ function createItem(req){
 	};
 }
 
+function getItem(params) {
+	console.log(params)
+	if (params.id === 'getall') {
+		return [
+			{
+				"user_id": faker.id,
+				"item_id": faker.id,
+				"irem_name": faker.commerce.productName(),
+				"address": faker.address.streetAddress()
+			},
+			{
+				"id": faker.id,
+				"user_name": faker.name.firstName(),
+				"email": faker.internet.email()
+			},
+			{
+				"id": faker.id,
+				"user_name": faker.name.firstName(),
+				"email": faker.internet.email()
+			},
+			{
+				"id": faker.id,
+				"user_name": faker.name.firstName(),
+				"email": faker.internet.email()
+			}
+		]
+	} else if (params.id === 'create') {
+		return {
+			"status": 'success',
+		}
+	} else if (params.id === 'delete') {
+		return {
+			"status": 'success',
+		}
+	} else {
+		return {
+			"id": params.id,
+			"user_name": faker.name.firstName(),
+			"email": faker.internet.email()
+		}
+	}
+}
+
 module.exports = {
 	getUser,
 	updateUser,
 	login,
 	createItem,
+
 }
--- project_code/server.js
+++ project_code/server.js
@@ -40,3 +40,40 @@ app.post('/item/create', (req, res) => {
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
+
+//Add more api
+app.post('/logout', (req, res) => {
+	res.send(db.logout(req));
+})
+
+app.post('/user/create', (req, res) => {
+	console.log(req.body);
+	res.send(db.createUser(req));
+})
+
+app.post('/user/view/delete/:id', (req, res) => {
+	res.send(db.deleteUser(req));
+})
+
+app.get('/user/view/getall', (req, res) => {
+	console.log(req.params);
+	res.send(db.getUser(req.params));
+})
+
+app.post('/item/delete/:id', (req, res) => {
+	res.send(db.deleteItem(req));
+})
+
+app.post('/item/update/:id', (req, res) => {
+	res.send(db.updateItem());
+})
+
+app.get('/item/view/:id', (req, res) => {
+	console.log(req.params);
+	res.send(db.getItem(req.params));
+})
+
+app.get('/user/getall', (req, res) => {
+	console.log(req.params);
+	res.send(db.getAllItem(req.params));
+})
