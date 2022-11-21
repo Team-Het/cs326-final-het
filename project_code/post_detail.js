46561ae587d36a69ee00dd83fa69f33170a3d657
Kelly Chung <kechung@umass.edu>
2022-11-21 02:00:40 -0500

    fix post page to post detail bug

--- project_code/post_detail.js
+++ project_code/post_detail.js
@@ -48,7 +48,7 @@ function goUpdate() {
 // Post Detail functions
 window.onload = function () {
     refreshSign();
-    const body = JSON.parse(localStorage.getItem('passBody'));
+    const body = JSON.parse(localStorage.getItem('item_detail'));
     console.log(body);
 
     if (body.is_found === 'n') {
