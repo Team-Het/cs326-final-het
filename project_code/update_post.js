d4362c7eae577ab7755bb3b3dfafb13a1da13005
Kelly Chung <kechung@umass.edu>
2022-11-20 21:38:10 -0500

    add sign out sign in functionality for update_post

--- project_code/update_post.js
+++ project_code/update_post.js
@@ -8,10 +8,30 @@ function goHome() {
 }
 
 function goProfile(){
-    window.location.href = "./profile.html";
+    const username = localStorage.getItem('username');
+    if(username){
+        window.location.href = "./profile.html";
+    } else {
+        localStorage.setItem('nextPage', 'profile.html');
+        window.location.href = "./login.html";
+    }
 }
 
+function refreshSign(){
+    const username = localStorage.getItem("username");
+    const signIn = document.getElementById("signIn");
+    const signOut = document.getElementById("signOut");
+    if(username){
+        signIn.classList.add('hidden');
+        signOut.classList.remove('hidden');
+    } else {
+        signIn.classList.remove('hidden');
+        signOut.classList.add('hidden');
+    }
+} 
+
 window.onload = async function () {
+	refreshSign();
 	const updateBody = JSON.parse(localStorage.getItem('updateBody'));
     document.getElementById('title').value = updateBody.item_name;
 	document.getElementById('category').value = updateBody.category;
